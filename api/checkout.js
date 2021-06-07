ed9afb55cbd9abb818111af07d2db4d04fba052e
ddurzo <dario.durzo@brunellocucinelli.it>
2021-06-07 12:42:17 +0200

    - fix url

--- api/checkout.js
+++ api/checkout.js
@@ -1,5 +1,5 @@
 const stripe = require("stripe")(process.env.STRIPE_TEST_SK);
-const hostUrl = "https://nuxt-stripe-amber.vercel.app";
+const hostUrl = "https://buy-nothing-challenge.vercel.app/";
 
 export default async (req, res) => {
   const session = await stripe.checkout.sessions.create({
